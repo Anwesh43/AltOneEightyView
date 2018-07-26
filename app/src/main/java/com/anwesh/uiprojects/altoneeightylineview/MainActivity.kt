b856bfce0cb82af238735d0dc2b106fbd97254fa
Anwesh43 <anweshthecool0@gmail.com>
2018-07-27 02:04:57 +0530

    adding AOELView in Activity

--- app/src/main/java/com/anwesh/uiprojects/altoneeightylineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/altoneeightylineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.altoneeightylineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.aoelview.AOELView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AOELView.create(this)
     }
 }
