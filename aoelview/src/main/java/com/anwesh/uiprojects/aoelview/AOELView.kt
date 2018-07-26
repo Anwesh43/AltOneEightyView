bf6853dd4b35b234f8cffb5996c07cd66be0c51e
Anwesh43 <anweshthecool0@gmail.com>
2018-07-27 01:21:08 +0530

    adding aoelview module

---
+++ aoelview/src/main/java/com/anwesh/uiprojects/aoelview/AOELView.kt
@@ -0,0 +1,13 @@
+package com.anwesh.uiprojects.aoelview
+
+/**
+ * Created by anweshmishra on 27/07/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+
+val NODES : Int = 5
