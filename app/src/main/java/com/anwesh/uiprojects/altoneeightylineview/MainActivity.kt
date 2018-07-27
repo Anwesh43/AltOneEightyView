c3bad9b78afdd9364fda90513989ace34dd83a6d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-27 10:37:22 +0530

    adding OnAnimationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/altoneeightylineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/altoneeightylineview/MainActivity.kt
@@ -3,16 +3,31 @@ package com.anwesh.uiprojects.altoneeightylineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.aoelview.AOELView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AOELView.create(this)
+        val view : AOELView = AOELView.create(this)
         fullScreen()
+        view.addOnAnimationCompleteListener({createCompleteToast(it)}, {createResetToast(it)})
+    }
+
+    fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun createCompleteToast(i : Int) {
+        createToast("complete ${i}")
+    }
+
+    fun createResetToast(i : Int) {
+        createToast("reset ${i}")
     }
 }
+
 
 fun MainActivity.fullScreen() {
     supportActionBar?.hide()
